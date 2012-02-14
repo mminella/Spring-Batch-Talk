cf24164a33cbd9b882b040eb6b44762a3c07fd8b
Michael Minella <michael@michaelminella.com>
2012-02-14 00:39:24 -0600

    Added Spring Batch Admin example

---
+++ examples/adminJob/src/main/java/com/michaelminella/springbatch/domain/Customer.java
@@ -0,0 +1,58 @@
+package com.michaelminella.springbatch.domain;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String city;
+	private String state;
+	private String zip;
+	
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getCity() {
+        return city;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public String getState() {
+        return state;
+    }
+    public void setState(String state) {
+        this.state = state;
+    }
+    public String getZip() {
+        return zip;
+    }
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+    @Override
+    public String toString() {
+        StringBuilder output = new StringBuilder();
+
+        output.append(firstName + " " + lastName + "\n");
+        output.append(address + "\n");
+        output.append(city + ", " + state + "\n");
+        output.append(zip);
+
+        return output.toString();
+    }
+}
